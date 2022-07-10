8b6266ad3d0e0d2257d14e1109eecbcb6ffea462
VgionTech <(0$t\U9i9b.{3k*>
2022-07-10 21:19:30 +0530

    init

---
+++ app/src/main/java/com/example/modules/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.modules
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.commonmodule.FirstActivity
+import com.squareup.picasso.Picasso
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
